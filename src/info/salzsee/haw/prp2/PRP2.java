58a1e6580f90f11cd79b8b09082bfd6d5c51deb5
Ben <salzig@gmail.com>
2011-04-05 13:32:04 +0200

    kill parseArgs, and use fixed size canvas

--- src/info/salzsee/haw/prp2/PRP2.java
+++ src/info/salzsee/haw/prp2/PRP2.java
@@ -1,7 +1,9 @@
 package info.salzsee.haw.prp2;
 
+import jgame.JGPoint;
+
 public class PRP2 {
 	public static void main(String[] args) {
-		new Simulator(args);
+		new Simulator(new JGPoint(960,480));
 	}
 }
--- src/info/salzsee/haw/prp2/Simulator.java
+++ src/info/salzsee/haw/prp2/Simulator.java
@@ -4,16 +4,17 @@ import jgame.*;
 import jgame.platform.*;
 
 public class Simulator  extends StdGame {
+	private JGPoint size;
 
-	public Simulator(String[] args) {
-		JGPoint size = parseSizeArgs(args,0);
+	public Simulator(JGPoint size) {
+		this.size = size;
 		initEngine(size.x,size.y);
 	}
 	
 	@Override
 	public void initCanvas() {
 		// setCanvasSettings(TilesX,TilesY,TileW,TileH,FGColor,BGColor,MsgFont)
-		setCanvasSettings(60,60,16,16,null,JGColor.blue,null);
+		setCanvasSettings(size.x/16,size.y/16,16,16,null,JGColor.blue,null);
 	}
 
 	@Override
