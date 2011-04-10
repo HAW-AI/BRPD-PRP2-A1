f994ae58209e4d8831174f8ec4a8665879a62ece
Ben <salzig@gmail.com>
2011-04-10 17:08:14 +0200

    remove paint method from Simulatable interface

--- src/de/hawhamburg/informatik/ai/prp2/interfaces/Simulatable.java
+++ src/de/hawhamburg/informatik/ai/prp2/interfaces/Simulatable.java
@@ -2,7 +2,6 @@ package de.hawhamburg.informatik.ai.prp2.interfaces;
 
 public interface Simulatable {
 	public void simulate(Double time);
-	public void paint();
 	public Double getPosition(); 
 	public String getTexture();
 	public Double getSpeed();
