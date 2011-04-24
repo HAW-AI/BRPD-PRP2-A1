52de0f39ae1a7c2ad1e26644eec92f6bafb65c1d
Ben <salzig@gmail.com>
2011-04-25 00:48:59 +0200

    git add LevelAfflicted interface

---
+++ src/de/hawhamburg/informatik/ai/prp2/interfaces/LevelAfflicted.java
@@ -0,0 +1,8 @@
+package de.hawhamburg.informatik.ai.prp2.interfaces;
+
+public interface LevelAfflicted {
+	public void setLevel(Double level);
+	public Double getLevel();
+	public void increaseLevel();
+	public void decreaseLeveL();
+}
