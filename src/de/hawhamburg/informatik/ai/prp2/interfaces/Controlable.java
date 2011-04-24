759206fc3780386fe25e1efc30d430fbe1abb649
Ben <salzig@gmail.com>
2011-04-25 00:48:44 +0200

    add controlable interface

---
+++ src/de/hawhamburg/informatik/ai/prp2/interfaces/Controlable.java
@@ -0,0 +1,8 @@
+package de.hawhamburg.informatik.ai.prp2.interfaces;
+
+public interface Controlable {
+	public void onKeyDown();
+	public void onKeyUp();
+	public void onKeyLeft();
+	public void onKeyRight();
+}
